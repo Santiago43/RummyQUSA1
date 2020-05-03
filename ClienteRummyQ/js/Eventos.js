019aec6aad85bab0d3254554cc3187f2d991cac3
unknown <santipego0001@gmail.com>
2020-05-03 12:07:14 -0500

    Cambios en eventos

--- ClienteRummyQ/js/Eventos.js
+++ ClienteRummyQ/js/Eventos.js
@@ -1,7 +1,12 @@
 $("#BtnCrearSala").click(function(){
+	setCookie("nombre",$("#nombre").val(),1);
 	crearSala();
-})
+});
 
 $("#StartGame").click(function(){
 	iniciarPartida();
-})
+});
+
+$("#UnirseASalarse").click(function(){
+	setCookie("nombre",$("#nombre").val(),1);
+});
