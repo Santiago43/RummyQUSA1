4448595a0f76c52d0b0affacc3b9036fb5f2c82a
unknown <santipego0001@gmail.com>
2020-05-03 14:41:47 -0500

    Servidor

--- RummyQ/src/servidor/Servidor.java
+++ RummyQ/src/servidor/Servidor.java
@@ -53,6 +53,7 @@ public class Servidor extends WebSocketServer {
 
     @Override
     public void onMessage(WebSocket ws, String mensaje) {
+        System.out.println("Mensaje entrante "+mensaje);
         JSONObject obj = new JSONObject(mensaje);
         String tipo = (String) obj.get("tipo");
         Usuario usuario = null;
@@ -63,30 +64,36 @@ public class Servidor extends WebSocketServer {
                 ws.send("pong");
                 break;
             case "crear sala":
+                System.out.println("Servidor: creando sala");
                 int numeroSala = Generador.generarNumeroSala();
                 sala = new Sala(numeroSala, "Sala: " + numeroSala);
                 this.salas.add(sala);
                 usuario = Buscador.buscarUsuario(ws, usuarios);
+                System.out.println("Servidor: "+usuario.toString());
                 if (usuario == null) {
                     System.out.println("Error");
                     objeto = "{\"tipo\":\"error\",\"mensaje\":\"ese usuario no existe\"}";
                     ws.send(objeto);
                 } else {
-                    usuario.setNombre((String) obj.get("nombre"));
-                    objeto = "{\"tipo\":\"codigo sala\",\"código\":\"" + sala.getCodigo() + "\"}";
+                    usuario.setNombre(obj.getString("usuario"));
+                    System.out.println(usuario.getNombre());
+                    objeto = "{\"tipo\":\"codigo sala\",\"codigo\":\"" + sala.getCodigo() + "\",\"mensaje\":\"conectado\"}";
+                    System.out.println("Sala creada: "+numeroSala);
                     usuario.setSala(sala);
+                    sala.getUsuarios().add(usuario);
                     ws.send(objeto);
                 }
                 break;
             case "conectarse a sala":
                 int codigo = obj.getInt("codigo");
                 usuario = Buscador.buscarUsuario(ws, usuarios);
+                usuario.setNombre(obj.getString("usuario"));
                 sala = Buscador.buscarSala(codigo, salas);
                 usuario.setSala(sala);
                 sala.getUsuarios().add(usuario);
-                objeto = "{\"tipo\":\"nuevo jugador\"}";
+                objeto = "{\"tipo\":\"nuevo jugador\",\"participantes\":\""+(sala.getUsuarios().size()+1)+"\"}";
                 sala.enviarATodosEnSalaExceptoA(usuario.getWebSocket(), objeto);
-                objeto = "{\"tipo\":\"participantes\",\"participantes\":\"" + sala.getUsuarios().size() + "\"}";
+                objeto = "{\"tipo\":\"conexion sala\",\"participantes\":\"" + (sala.getUsuarios().size()+1) + "\",\"mensaje\":\"conectado\"}";
                 ws.send(objeto);
                 break;
             case "desconectarse de sala":
@@ -136,7 +143,7 @@ public class Servidor extends WebSocketServer {
      */
     @Override
     public void onError(WebSocket ws, Exception excptn) {
-        System.out.println("");
+        System.out.println(excptn);
     }
 
 }
