867f937882077af89d44ea6bdc0b137ec594729d
unknown <santipego0001@gmail.com>
2020-05-05 18:29:37 -0500

    Cliente

--- ClienteRummyQ/js/conexion.js
+++ ClienteRummyQ/js/conexion.js
@@ -1,36 +1,36 @@
 /**
- * Promesa
- * @param {*} ms 
- */
+* Promesa
+* @param {*} ms 
+*/
 var wait = ms => new Promise((r, j) => setTimeout(r, ms));
 /**
- * Dirección con protocolo ws
- */
+* Dirección con protocolo ws
+*/
 var wsUri = "ws://localhost:30001";
 /**
- * Websocket
- */
+* Websocket
+*/
 var websocket = new WebSocket(wsUri);
 /**
- * Mano del jugador
- */
+* Mano del jugador
+*/
 var mano= new Array();
 /**
- * Lista de jugadores
- */
+* Lista de jugadores
+*/
 var jugadores = new Array();
 /**
- * El turno
- */
+* El turno
+*/
 var turno ={valor:false};
 var posicion;
 /**
- * El origen de una jugada
- */
+* El origen de una jugada
+*/
 var origen;
 /**
- * id
- */
+* id
+*/
 var coords;
 
 /**
@@ -76,18 +76,18 @@ websocket.onmessage=function(event){
 		else if(obj.tipo==="nueva mano"){
 			cambiarAlTablero(obj.mano,obj.jugadores)
 		}
-		else if(obj.tipo==="robar ficha"){
+		else if(obj.tipo==="ficha robada"){
 			fichaRobada(obj.ficha);
 		}
 		else if(obj.tipo==="cambio turno"){
 			cambiarTurno(obj.valor);
-        }
-        else if(obj.tipo==="colocar ficha"){
+		}
+		else if(obj.tipo==="colocar ficha"){
 			fichaColocada(obj.ficha);
-        }
-        else if(obj.tipo==="mover ficha"){
+		}
+		else if(obj.tipo==="mover ficha"){
 			fichaMovida(obj.ficha);
-        }
+		}
 		else if(obj.tipo==="turno"){
 			jugadorEnTurno(obj.jugador);
 		}
@@ -134,12 +134,12 @@ function enviarMensaje(object){
 
 
 /**
- * Aquí comienzan las funciones de SALA
- * 
- * Todas las funciones acá abajo representan cada una de las operaciones
- * que se pueden realizar desde el cliente hacia el servidor
- * 
- */
+* Aquí comienzan las funciones de SALA
+* 
+* Todas las funciones acá abajo representan cada una de las operaciones
+* que se pueden realizar desde el cliente hacia el servidor
+* 
+*/
 
 
 /**
@@ -149,53 +149,53 @@ function crearSala(){
 	var object ={tipo: "crear sala", usuario: getCookie("nombre")};
 	enviarMensaje(object);
 	/**
-	 * Luego se debe colocar el cambio al tablero de este usuario, lo cual 
-	 * se da en una noficiación del servidor
-	 */
-	}
+	* Luego se debe colocar el cambio al tablero de este usuario, lo cual 
+	* se da en una noficiación del servidor
+	*/
+}
 /**
- * Función que permite conectarse a una sala
- */
- function conectarASala(codigo,usuario){
- 	var object= {tipo: "conectarse a sala", codigo: codigo,usuario: usuario};
- 	enviarMensaje(object);
- }
+* Función que permite conectarse a una sala
+*/
+function conectarASala(codigo,usuario){
+	var object= {tipo: "conectarse a sala", codigo: codigo,usuario: usuario};
+	enviarMensaje(object);
+}
 
 /**
- * Función que agrega participantes a la sala
- * @param {number} participantes que es el número nuevo de participantes
- */
- function añadirParticipante(participantes){
+* Función que agrega participantes a la sala
+* @param {number} participantes que es el número nuevo de participantes
+*/
+function añadirParticipante(participantes){
 	$("#noParticipantes").empty();
- 	$("#ParticipantesInvitados").empty();
- 	$("#noParticipantes").append("Participantes: "+participantes);
- 	$("#ParticipantesInvitados").append("Participantes: "+participantes);
-
- }
+	$("#ParticipantesInvitados").empty();
+	$("#noParticipantes").append("Participantes: "+participantes);
+	$("#ParticipantesInvitados").append("Participantes: "+participantes);
+	
+}
 
 /**
- * Función que permite conectarse a la sala de espera. 
- * La misma puede es invocada desde una respuesta del servidor
- * @param {number} codigo que es el código de la sala
- * @param {String} mensaje que es un mensaje del servidor
- * @param {number} participantes que son los participantes activos en ese momento
- */
- function cambiarASalaDeEsperaCreador(codigo,mensaje,participantes){
- 	$("#DivCrearSala").show();
- 	$("#DivInicio").hide();
- 	$("#InputCodigo").attr("value",codigo);
- 	console.log(mensaje);
- 	$("#noParticipantes").append(participantes);
- }
+* Función que permite conectarse a la sala de espera. 
+* La misma puede es invocada desde una respuesta del servidor
+* @param {number} codigo que es el código de la sala
+* @param {String} mensaje que es un mensaje del servidor
+* @param {number} participantes que son los participantes activos en ese momento
+*/
+function cambiarASalaDeEsperaCreador(codigo,mensaje,participantes){
+	$("#DivCrearSala").show();
+	$("#DivInicio").hide();
+	$("#InputCodigo").attr("value",codigo);
+	console.log(mensaje);
+	$("#noParticipantes").append(participantes);
+}
 
 
- function cambiarASalaDeEspera(codigo,mensaje,participantes){
- 	$("#DivInformacionSala").show();
- 	$("#DivBotonUnirse").hide();
- 	$("#InputCodigo2").attr("value",codigo);
- 	console.log(mensaje);
- 	$("#ParticipantesInvitados").append(participantes);
- }
+function cambiarASalaDeEspera(codigo,mensaje,participantes){
+	$("#DivInformacionSala").show();
+	$("#DivBotonUnirse").hide();
+	$("#InputCodigo2").attr("value",codigo);
+	console.log(mensaje);
+	$("#ParticipantesInvitados").append(participantes);
+}
 
 /**
 * Función que permite iniciar la partida
@@ -206,44 +206,40 @@ function iniciarPartida(){
 }
 
 /**
- * Función que permite cambiar al tablero
- * @param {*} nuevaMano que es el arreglo con la mano del jugador 
- */
- function cambiarAlTablero(nuevaMano,nuevosJugadores){
- 	$("#DivInicio").hide();
- 	$("#DivCrearSala").hide();
- 	$("#DivUnirseASala").hide();
- 	$("#DivInformacionSala").hide();
- 	$("#gridTablero").show();		
-
- 	var texto="";
- 	var fichas="";
- 	for (let i = 0; i < nuevaMano.length; i++) {
- 		mano.push(nuevaMano[i]);
- 		fichas='<div oncontextmenu="soni'+mano[i].color+''+mano[i].numero+'.play()" class="fill '+mano[i].color+'-'+mano[i].numero+'" draggable="true"> <img src="img/fichas/'+mano[i].color+'-'+mano[i].numero+'.png" height="70px" width="43px" ></div>';
- 		$("#"+i).append(fichas);
- 	}	
- 	for (let i = 0; i < nuevosJugadores.length; i++) {
- 		jugadores.push(nuevosJugadores[i]); 	
+* Función que permite cambiar al tablero
+* @param {*} nuevaMano que es el arreglo con la mano del jugador 
+*/
+function cambiarAlTablero(nuevaMano,nuevosJugadores){
+	$("#DivInicio").hide();
+	$("#DivCrearSala").hide();
+	$("#DivUnirseASala").hide();
+	$("#DivInformacionSala").hide();
+	$("#gridTablero").show();		
+	
+	var texto="";
+	var fichas="";
+	for (let i = 0; i < nuevaMano.length; i++) {
+		mano.push(nuevaMano[i]);
+		fichas='<div oncontextmenu="soni'+mano[i].color+''+mano[i].numero+'.play()" class="fill '+mano[i].color+'-'+mano[i].numero+'" draggable="true"> <img src="img/fichas/'+mano[i].color+'-'+mano[i].numero+'.png" height="70px" width="43px" ></div>';
+		$("#"+i).append(fichas);
+	}	
+	for (let i = 0; i < nuevosJugadores.length; i++) {
+		jugadores.push(nuevosJugadores[i]); 	
 		texto='<div class="casillaJugador"><img src="img/'+(i+1)+'.png">'+nuevosJugadores[i].nombre+'</div>';	
 		$("#jugadores").append(texto);
-	 }
-	 $( function() {
+	}
+	$( function() {
 		$( ".fill" ).draggable({
 			stop: function(event,ui){
-                console.log("stop");
+				console.log("stop");
 			},
 			start:function(event,ui){
-                console.log("start");
-                //console.log($(this).parent().parent());
+				console.log("start");
+				//console.log($(this).parent().parent());
 				var variable = $(this).parent().parent().parent();
 				posicion = $(this).parent();
-				console.log(posicion);
 				coords = $(posicion).attr("id");
-				console.log(coords);
-				console.log($(variable[0]).attr("id"));
 				origen=$(variable[0]).attr("id");
-                console.log($(this).parent());
 			},
 			revert:function(posicion){
 				console.log(nuev);
@@ -252,7 +248,7 @@ function iniciarPartida(){
 		});
 		$( ".empty.column" ).droppable({
 			drop: function(event,ui){
-                
+				
 				ui.draggable.addClass("dropped");
 				console.log("drop");
 				$(this).append(ui.draggable);
@@ -265,23 +261,7 @@ function iniciarPartida(){
 				}
 				else if(origen ==="tablero"){
 					moverFicha(event.target.id,valorFicha,coords);
-				}
-                posicion = ui.draggable;
-				
-				console.log(id);
-				/*
-                var ficha = datos.split("-");
-                
-				if(true){
-                    colocarFicha(event.target.id,ficha);
-                }
-				else{
-                    var posicionAnterior; //La posible posición ;c
-                    moverFicha(event.target.id,ficha,posicionAnterior);
-                }*/
-                    
-                
-				console.log(posicion);				
+				}				
 			}
 		});
 		$(".empty.espacioMano").droppable({
@@ -289,31 +269,30 @@ function iniciarPartida(){
 				
 			}
 		})
-	
+		
 	} );
-	//Luego irán las instrucciones necesarias para hacer el cambio de la página al tablero
 }
 
 
 /**
- * A continuación, las funciones que representan jugadas del cliente. 
- * Estas permiten lograr la sincronización con el servidor y las jugadas que realiza
- * este y los demás clientes.
- */
+* A continuación, las funciones que representan jugadas del cliente. 
+* Estas permiten lograr la sincronización con el servidor y las jugadas que realiza
+* este y los demás clientes.
+*/
 
 
 /**
- * Función que permite mover una ficha
- * @param {*} idDiv es el div de la posición actual
- * @param {*} ficha ficha
- * @param {*} prev posición previa
- */
+* Función que permite mover una ficha
+* @param {*} idDiv es el div de la posición actual
+* @param {*} ficha ficha
+* @param {*} prev posición previa
+*/
 function moverFicha(idDiv,ficha,prev){
-    var coordenadas = idDiv.split("-");
-    var x = coordenadas[0];
-    var y = coordenadas[1];
-    var color = ficha[0];
-    var numero = ficha[1];
+	var coordenadas = idDiv.split("-");
+	var x = coordenadas[0];
+	var y = coordenadas[1];
+	var color = ficha[0];
+	var numero = ficha[1];
 	var coordenadasPrevias = prev.split("-");
 	var xAnterior = coordenadasPrevias[0];
 	var yAnterior = coordenadasPrevias[1];
@@ -332,19 +311,19 @@ function moverFicha(idDiv,ficha,prev){
 }
 
 /**
- * Función que permite colocar una ficha
- * @param {*} idDiv que es el id del div donde está la ficha 
- * @param {*} valorFicha que es la ficha
- */
+* Función que permite colocar una ficha
+* @param {*} idDiv que es el id del div donde está la ficha 
+* @param {*} valorFicha que es la ficha
+*/
 function colocarFicha(idDiv, valorFicha){
-    var coordenadas = idDiv.split("-");
-    var x = coordenadas[0];
-    var y = coordenadas[1];
-    var color = valorFicha[0];
-    var numero = valorFicha[1];
-    var objeto ={
-        tipo: "jugada - colocar ficha",
-        ficha: {
+	var coordenadas = idDiv.split("-");
+	var x = coordenadas[0];
+	var y = coordenadas[1];
+	var color = valorFicha[0];
+	var numero = valorFicha[1];
+	var objeto ={
+		tipo: "jugada - colocar ficha",
+		ficha: {
 			x:x,
 			y:y,
 			color:color,
@@ -354,26 +333,26 @@ function colocarFicha(idDiv, valorFicha){
 		}
 	}
 	console.log(objeto);
-    enviarMensaje(objeto);
+	enviarMensaje(objeto);
 }
 
 
 /**
- * Función que permite terminar la partida. El jugador es retornado a la 
- * Vista por la que entro
- * @param {*} ganador que es el nombre de quien ganó la partida
- */
+* Función que permite terminar la partida. El jugador es retornado a la 
+* Vista por la que entro
+* @param {*} ganador que es el nombre de quien ganó la partida
+*/
 
- function terminarJuego(ganador){
- 	alert("El juego terminó. El ganador es: "+ganador);
+function terminarJuego(ganador){
+	alert("El juego terminó. El ganador es: "+ganador);
 	//Luego se devuelve a la vista anterior 
 	$("#DivInicio").show();
 	$("#gridTablero").empty();
 }
 
 /**
- * Función que envía al servidor el fin de un turno
- */
+* Función que envía al servidor el fin de un turno
+*/
 function terminarTurno(){
 	var object ={
 		tipo: "terminar turno"
@@ -382,8 +361,8 @@ function terminarTurno(){
 }
 
 /**
- * Función que envía al servidor el robar una ficha
- */
+* Función que envía al servidor el robar una ficha
+*/
 function robarFicha(){
 	var object = {
 		tipo: "robar ficha"
@@ -392,9 +371,9 @@ function robarFicha(){
 }
 
 /**
- * Función que guarda la ficha robada de la banca
- * @param {Ficha} ficha que es la ficha robada
- */
+* Función que guarda la ficha robada de la banca
+* @param {Ficha} ficha que es la ficha robada
+*/
 function fichaRobada(ficha){
 	mano.push(ficha);
 	var continuar = true;
@@ -404,40 +383,84 @@ function fichaRobada(ficha){
 			var texto = '<div oncontextmenu="soni'+ficha.color+''+ficha.numero+'.play()" class="fill '+ficha.color+'-'+ficha.numero+'" draggable="true"> <img src="img/fichas/'+ficha.color+'-'+ficha.numero+'.png" height="70px" width="43px" ></div>';
 			$("#"+i).append(texto);
 			continuar=false;
+			$( function() {
+				$( ".fill" ).draggable({
+					stop: function(event,ui){
+						console.log("stop");
+					},
+					start:function(event,ui){
+						console.log("start");
+						//console.log($(this).parent().parent());
+						var variable = $(this).parent().parent().parent();
+						posicion = $(this).parent();
+						coords = $(posicion).attr("id");
+						origen=$(variable[0]).attr("id");
+					},
+					revert:function(posicion){
+						console.log(nuev);
+						return nuev;
+					}
+				});
+				$( ".empty.column" ).droppable({
+					drop: function(event,ui){
+						
+						ui.draggable.addClass("dropped");
+						console.log("drop");
+						$(this).append(ui.draggable);
+						var id = ui.draggable.attr('class');
+						var datos = id.split(" ");
+						var valorFicha = datos[1].split("-");
+						console.log(valorFicha);
+						if(origen==="manoJugador"){
+							colocarFicha(event.target.id,valorFicha);
+						}
+						else if(origen ==="tablero"){
+							moverFicha(event.target.id,valorFicha,coords);
+						}				
+					}
+				});
+				$(".empty.espacioMano").droppable({
+					drop: function(event,ui){
+						
+					}
+				})
+				
+			} );
+			
 		}else{
 			i++;
 		}
 	}
 }
 /**
- * Función que modifica el valor del objeto que indica si está en turno o no
- * @param {boolean} valor el estado del turno (sí o no)
- */
+* Función que modifica el valor del objeto que indica si está en turno o no
+* @param {boolean} valor el estado del turno (sí o no)
+*/
 function cambiarTurno(valor){
 	turno.valor=valor;
 }
 
 /**
- * Función que muestra quién está en turno
- */
+* Función que muestra quién está en turno
+*/
 function jugadorEnTurno(jugador){
 	alert("Turno de: "+jugador);
 }
 
 /**
- * 
- * @param {*} ficha 
- */
+* 
+* @param {*} ficha 
+*/
 function fichaColocada(ficha){
-    var texto = '<div oncontextmenu="soni'+ficha.color+''+ficha.numero+'.play()" class="fill '+ficha.color+'-'+ficha.numero+'" draggable="true"> <img src="img/fichas/'+ficha.color+'-'+ficha.numero+'.png" height="70px" width="43px" ></div>';
-    $("#"+ficha.x+"-"+ficha.y).append(texto);
+	var texto = '<div oncontextmenu="soni'+ficha.color+''+ficha.numero+'.play()" class="fill '+ficha.color+'-'+ficha.numero+'" draggable="true"> <img src="img/fichas/'+ficha.color+'-'+ficha.numero+'.png" height="70px" width="43px" ></div>';
+	$("#"+ficha.x+"-"+ficha.y).append(texto);
 }
 
 /**
- * @param {*} ficha
- */
+* @param {*} ficha
+*/
 function fichaMovida(ficha){
 	$("#"+ficha.xAnterior+"-"+ficha.yAnterior).empty();
 	var texto = '<div oncontextmenu="soni'+ficha.color+''+ficha.numero+'.play()" class="fill '+ficha.color+'-'+ficha.numero+'" draggable="true"> <img src="img/fichas/'+ficha.color+'-'+ficha.numero+'.png" height="70px" width="43px" ></div>';
-    $("#"+ficha.x+"-"+ficha.y).append(texto);
+	$("#"+ficha.x+"-"+ficha.y).append(texto);
 }
